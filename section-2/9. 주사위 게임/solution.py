75f55c427fde80345531fcef94a3793d5960b4bf
Seung Yeon Baek <chris8w8@naver.com>
2021-05-03 01:57:43 +0900

    다중 조건문

---
+++ section-2/9. 주사위 게임/solution.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.stdin = open("in1.txt", "rt")
+n = int(input())
+prices = [0] * n
+
+for i in range(n):
+    a, b, c = map(int, input().split())
+    if a == b and b == c:
+        prices[i] = 10000 + a * 1000
+    elif a == b or a == c:
+        prices[i] = 1000 + a * 100
+    elif b == c:
+        prices[i] = 1000 + b * 100
+    else:
+        prices[i] = max(a, b, c) * 100
+
+print(max(prices))
