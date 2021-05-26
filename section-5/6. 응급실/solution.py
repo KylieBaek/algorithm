61cc178c1db8b2ee6a77e830a9058ea08a789b8d
Seung Yeon Baek <chris8w8@naver.com>
2021-05-26 21:59:29 +0900

    enumerate list , 우선 순위 환자 queue

---
+++ section-5/6. 응급실/solution.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+
+sys.stdin = open("in1.txt", "rt")
+# 우선순위 환자 먼저 pop
+n, m = map(int, input().split())
+q = [(pos, val) for pos, val in enumerate(list(map(int, input().split())))]
+q = deque(q)
+cnt = 0
+
+while q:
+    cur = q.popleft()
+    if any(cur[1] < x[1] for x in q):
+        q.append(cur)
+    else:
+        cnt += 1
+        if cur[0] == m:
+            break
+
+print(cnt)
